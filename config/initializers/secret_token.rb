3d00f0a7f8877bbcba83011d49cf24b21b8e8ff8
chetan <chetan@idyllic-software.com>
2011-10-28 23:03:46 +0530

    first commite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skoobit::Application.config.secret_token = 'd7f9ccbb306eb7f5c2739149c4caa44973f26104f22f843ad066819146a72bea4100ad1a88c38639ebc5b25ecb0a7aa08073d0a59a1b1896e85e88ec2be133f8'
