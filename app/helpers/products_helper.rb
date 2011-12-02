07897925356bacd669f110932bd498d61b8fa5a6
chetan <chetan@idyllic-software.com>
2011-12-03 01:11:53 +0530

    price method changes

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -16,9 +16,6 @@ module ProductsHelper
   def product_price(product_or_variant, options={})
     options.assert_valid_keys(:format_as_currency, :show_vat_text)
     options.reverse_merge! :format_as_currency => true, :show_vat_text => Spree::Config[:show_price_inc_vat]
-    puts "!!!!!!!!!!!!!"
-#    puts product_or_variant.price
-    puts "!!!!!!!!!!!!!"
     amount = product_or_variant.price rescue 0
     amount += Calculator::Vat.calculate_tax_on(product_or_variant) if Spree::Config[:show_price_inc_vat]
     options.delete(:format_as_currency) ? format_price(amount, options) : amount
